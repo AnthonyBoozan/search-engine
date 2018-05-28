836cbeb680d094d616bcff8022729bf50be3a069
AnthonyBoozan <aboozan@uci.edu>
2018-05-28 15:58:51 -0700

    help

--- src/App.js
+++ src/App.js
@@ -2,17 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+  constructor() {
+        super();
+        this.state = {
+            user_input: "",
+            list: {}
+        };
+    }
+  handleServer(){
+        jsonResponse = {};
+        var url = "ec2-54-67-39-41.us-west-1.compute.amazonaws.com:3000";
+        
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name:this.state.userInput})
+            
+        }).then((response) => response.json())
+        .then((responseJson) => {
+            const ordered = {};
+            Object.keys(responseJson).sort().forEach(function(key) {
+              ordered[key] = unordered[key];
+            });
+            this.setState({
+                    list = ordered,
+                });
+            
+        });
+  } 
+  handleUserInput(e) {
+        this.setState({userInput: e.target.value});
+    }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. hi
-        </p>
+        <form>
+            <input type="text" name="name" value={this.state.userinput} onChange={this.handleUserInput}></input>
+            <button type="button" onClick={this.handleServer}>Search</button>
+        </form>
       </div>
     );
   }
